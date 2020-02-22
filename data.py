95f3d1a25a29b41067820d6b6ed5ba1341d97ec1
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-02-22 14:30:52 +0200

    pusdo data

---
+++ data.py
@@ -0,0 +1,53 @@
+def Articles():
+
+    articles = [
+
+         {
+             'id': 1,
+            'title': 'Intro To React Js',
+            'auhtor': 'yazan al monshed',
+            'create': '23-34-2019',
+            'content': 'to start using react you should start with javascript and learn how to use it with tath ',
+
+        },
+         {
+             'id': 2,
+            'title': 'Intro To Vue Js',
+            'auhtor': 'yazan al monshed',
+            'create': '23-34-2019',
+            'content': 'to start using react you should start with javascript and learn how to use it with tath ',
+
+        },
+         {
+             'id': 3,
+            'title': 'Intro To Node Js',
+            'auhtor': 'yazan al monshed',
+            'create': '23-34-2019',
+            'content': 'to start using react you should start with javascript and learn how to use it with tath ',
+
+        },
+         {
+             'id': 4,
+            'title': 'Intro To Svelte Js',
+            'auhtor': 'yazan al monshed',
+            'create': '23-34-2019',
+            'content': 'to start using react you should start with javascript and learn how to use it with tath ',
+
+        },
+         {
+             'id': 5,
+            'title': 'Intro To React Js',
+            'auhtor': 'yazan al monshed',
+            'create': '23-34-2019',
+            'content': 'to start using react you should start with javascript and learn how to use it with tath ',
+
+        }, {
+             'id': 6,
+            'title': 'Intro To React Js',
+            'auhtor': 'yazan al monshed',
+            'create': '23-34-2019',
+            'content': 'to start using react you should start with javascript and learn how to use it with tath ',
+
+        }
+]  
+    return articles
